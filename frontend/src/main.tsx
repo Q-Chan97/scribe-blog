fce1ac673cb1c5c3fe2f963bab99c5bc96cadfec
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-15 13:22:44 -0500

    feat: Implement Auth Context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import { AuthProvider } from './AuthContext.tsx'
 
 import routes from './routes.tsx'
 
@@ -8,6 +9,8 @@ const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 )
