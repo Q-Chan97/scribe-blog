8f08b9c30286576d9ba3d1572ebfcc1450c38de1
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-14 23:22:52 -0500

    feat: token verify, login/logout user logic

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import { type Request, type Response, type NextFunction } from "express";
-
+import jwt from "jsonwebtoken";
+import { prisma } from "../db/prisma.js";
 import * as queries from "../db/queries.js";
 
 export async function createUser(req: Request, res: Response, next: NextFunction) {
@@ -18,4 +19,62 @@ export async function createUser(req: Request, res: Response, next: NextFunction
     } catch (err) {
         next(err)
     }
+}
+
+export function verifyToken(req: Request, res: Response, next: NextFunction) {
+   
+    const bearerHeader = req.headers['authorization']; // Gets token
+    
+    if (!bearerHeader) {
+        res.status(403).json({ message: "Forbidden" });
+        return;
+    }
+
+    const bearerToken = bearerHeader.split(" ")[1] // Split at at space and extract
+
+    if (!bearerToken) {
+        res.status(403).json({ message: "Forbidden" });
+        return;
+    }
+
+    try {
+        const decoded = jwt.verify(bearerToken, process.env.SECRET_KEY!);
+        req.user = decoded as Express.User; // Attach payload to req 
+        next();
+    } catch {
+        res.status(401).json({ message: "Invalid or expired token"})
+    }
+}
+
+export async function loginUser(req:Request, res: Response) {
+    const user = await prisma.user.findUnique({
+        where: {
+            username: req.body.username,
+        }
+    })
+
+    if (!user) {
+        res.status(404).json({ message: "User not found" });
+        return;
+    }
+
+    const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+
+    if (!passwordMatch) {
+        res.status(401).json({ message: "Password is incorrect" });
+        return;
+    }
+
+    jwt.sign({user: user}, process.env.SECRET_KEY!, {expiresIn: "3d"}, (err, token) => {
+        if (err) {
+            return res.status(500).json({ message: "Token signing failed" })
+        }
+        res.json({
+            token: token,
+        })
+    })
+}
+
+export async function logoutUser(req: Request, res: Response) {
+    res.status(200).json({ message: "Logged out" });
 }
