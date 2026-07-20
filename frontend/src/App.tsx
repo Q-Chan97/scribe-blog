aad03494a793317864471cc17199ac7c4e54e358
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-20 15:57:04 -0500

    feat: Implement outlet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import "./App.css";
 
+import { Outlet } from "react-router";
+
 import Nav from "./components/Nav/Nav.tsx";
 
 export default function App() {
+
   return (
-    <Nav />
+    <>
+      <Nav />
+      <main>
+        <Outlet />
+      </main>
+    </>
   )
 }
