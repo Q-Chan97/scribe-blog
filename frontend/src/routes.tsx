123f68e392118cc8ae922d59e7ee1b1106237917
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-20 16:01:04 -0500

    feat: Add children routes for Outlet

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,9 @@
 import App from "./App.tsx"
 import SignUp from "./components/SignUp/SignUp.tsx";
 import Login from "./components/Login/Login.tsx";
+import Main from "./components/Main/Main.tsx";
+import Blog from "./components/Blog/Blog.tsx";
+import CreatePost from "./components/CreatePost/CreatePost.tsx";
 
 import ForwardAuthenticated from "./Redirect/forwardAuth.tsx";
 
@@ -8,6 +11,11 @@ const routes = [
     {
         path: "/",
         element: <App />,
+        children: [
+            { index: true, element: <Main /> },
+            { path: ":userId/posts/:id", element: <Blog />},
+            { path: "create", element: <CreatePost />}
+        ]
     },
     {
         path: "/signUp",
