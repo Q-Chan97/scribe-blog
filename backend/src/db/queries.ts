c7e24e35c171ed2dfb3a94abc3fd7d687668751a
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-20 15:42:33 -0500

    feat: Create post and newest post queries

--- backend/src/db/queries.ts
+++ backend/src/db/queries.ts
@@ -21,3 +21,23 @@ export const createUser = async ({username, password, email}: reqTypes) => {
     });
     return user;
 }
+
+export const createBlogPost = async (title: string, content: string, authorId: number) => {
+    const post = await prisma.blogPost.create({
+       data: {
+        title: title,
+        text: content,
+        userId: authorId,
+       }
+    })
+    return post;
+}
+
+export const newestBlogPost = async (userId: number) => {
+    const post = await prisma.blogPost.findFirst({
+        where: { userId},
+        orderBy: { createdAt: "desc" },
+        include: { blogComments: true},
+    })
+    return post;
+}
