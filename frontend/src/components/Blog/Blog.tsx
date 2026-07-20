7303dd85f2ce531ab6cf319966a5e14c313eb9a7
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-20 15:42:54 -0500

    feat: Build blog component

---
+++ frontend/src/components/Blog/Blog.tsx
@@ -0,0 +1,33 @@
+import Comment from "../Comment/Comment.tsx"
+
+import { useEffect, useState } from "react";
+import { useParams } from "react-router"
+
+interface BlogProps {
+    blog?: any;
+}
+
+export default function Blog({blog: blogProp}: BlogProps) {
+    const { userId, id } = useParams();
+    const [blog, setBlog] = useState(blogProp || null);
+
+    useEffect(() => {
+        if (blogProp || !userId || !id) return; // Skip the fetch if data is passed in as props
+        fetch(`${import.meta.env.VITE_BACKEND_URL}/${userId}/posts/${id}`)
+            .then(res => res.json())
+            .then(data => setBlog(data));
+    }, [userId, id]);
+
+    return (
+        <section>
+            <div>
+                { blog?.title }
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: blog?.text || "" }} />
+            {blog?.replies && 
+                blog.blogComments.map((comment) => (
+                    <Comment text={comment.commentText} posted={comment.createdAt} replies={comment.childComments}/>
+                ))}
+        </section>
+    )
+}
