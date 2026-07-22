9e4091948e7e34e7c380af294b22ed4d27029059
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-21 22:17:28 -0500

    feat: Delete handler and buttons

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -17,6 +17,7 @@ interface SidebarProps {
 export default function Sidebar({ userId }: SidebarProps) {
     const { user } = useAuth();
     const [postList, setPostList] = useState<Post[]>([]); // Array for all posts
+    const [confirmDelete, setConfirmDelete] = useState<number | null>(null)
     const isOwner = user?.id === userId;
 
     useEffect(() => {
@@ -49,6 +50,23 @@ export default function Sidebar({ userId }: SidebarProps) {
         }
     }
 
+    async function handleDeleteBlogPost(post: Post) {
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/${user.id}/posts/${post.id}/delete`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            },
+        });
+
+        if (res.ok) {
+            setPostList(prev => prev.filter(p => 
+                p.id !== post.id
+            ))
+            setConfirmDelete(null);
+        }
+    }
+
     return (
         <section>
             <h3>Blog Posts</h3>
@@ -60,10 +78,24 @@ export default function Sidebar({ userId }: SidebarProps) {
                         </Link>
                         <p>{post.createdAt}</p>
                         {isOwner && (
-                            <button onClick={() => handleTogglePublished(post)}>
-                                {post.isPublished ? "Unpublish" : "Publish"}
-                            </button>
-
+                            <div>
+                                <button onClick={() => handleTogglePublished(post)}>
+                                    {post.isPublished ? "Unpublish" : "Publish"}
+                                </button>
+                                {confirmDelete === post.id ? (
+                                    <div>
+                                        <span>Do you want to delete this?</span>
+                                        <button onClick={() => setConfirmDelete(null)}>No</button>
+                                        <button onClick={() => handleDeleteBlogPost(post)}>
+                                            Delete
+                                        </button>
+                                    </div>
+                                ) : (
+                                    <button onClick={() => setConfirmDelete(post.id)}>
+                                        Delete
+                                    </button>
+                                )}
+                            </div>
                         )}
                     </li>
                 ))}
