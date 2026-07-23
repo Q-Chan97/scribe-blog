ca7e23a19efb5b6e4a5da237ea176c9f44baeceb
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-23 10:44:37 -0500

    feat: Pass comment key, username, and type

--- frontend/src/components/Comment/Comment.tsx
+++ frontend/src/components/Comment/Comment.tsx
@@ -1,11 +1,21 @@
-export default function Comment({text, posted, replies}) {
+
+interface Comment {
+    id: number
+    user: { username: string }
+    commentText: string
+    createdAt: Date
+    childComments: Comment[]
+}
+
+export default function Comment({id, username, text, posted, replies}) {
     return (
         <div>
+            <p>{username}</p>
             <p>{text}</p>
             <p>{posted}</p>
             {replies && 
-                replies.map((reply) => (
-                    <Comment text={reply.commentText} posted={reply.createdAt} replies={reply.childComments} />
+                replies.map((reply: Comment) => (
+                    <Comment key={reply.id} id={reply.id} username={reply.user.username} text={reply.commentText} posted={reply.createdAt} replies={reply.childComments} />
                 ))
             }
         </div>
