0b21cfbdfb34300553c9d76bfeb0a2826d7745b0
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-27 16:53:03 -0500

    feat: Implement delete comment handlers

--- frontend/src/components/Comment/Comment.tsx
+++ frontend/src/components/Comment/Comment.tsx
@@ -4,11 +4,11 @@ import { useState } from "react";
 import { useAuth } from "../../AuthContext.tsx";
 import { type CommentType } from "../../types.ts";
 
-export default function Comment({ id, username, commentText, createdAt, childComments, postId, userId, onReplyPosted }: CommentType) {
+export default function Comment({ id, username, commentText, createdAt, childComments, commentUserId, isDeleted, postId, userId, onReplyPosted, setBlogComments }: CommentType) {
     const [isReplying, setIsReplying] = useState(false);
     const [replyText, setReplyText] = useState("");
-    const { isLoggedIn } = useAuth();
-
+    const { isLoggedIn, user } = useAuth();
+    const isOwner = user?.id === commentUserId;
 
     async function handleReply() {
         if (!replyText) return;
@@ -31,10 +31,30 @@ export default function Comment({ id, username, commentText, createdAt, childCom
         }
     }
 
+    async function handleDelete(commentId: number) {
+        if (!commentId) return; 
+
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/${userId}/posts/${postId}/comments/${id}/delete`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            }
+        })
+
+        if (res.ok) {
+            setBlogComments(prev => prev.map((comment) => (
+                comment.id === commentId
+                ? {...comment, isDeleted: true}
+                : comment
+            )));
+        }
+    }
+
     return (
         <div className={styles.commentChain}>
             <div className={styles.commentContainer}>
-                <p className={styles.name}>{username}</p>
+                <p className={styles.name}>{isDeleted ? "[Deleted]" : username}</p>
                 <p className={styles.date}>{new Date(createdAt).toLocaleDateString("en-US", {
                     hour: "2-digit",
                     minute: "2-digit",
@@ -42,7 +62,7 @@ export default function Comment({ id, username, commentText, createdAt, childCom
                     month: "2-digit",
                     day: "2-digit",
                 })}</p>
-                <p>{commentText}</p>
+                <p>{isDeleted ? "[Comment Deleted]" : commentText}</p>
                 {isLoggedIn && (
                 isReplying ? (
                     <div className={styles.replyContainer}>
@@ -58,7 +78,14 @@ export default function Comment({ id, username, commentText, createdAt, childCom
                         </div>
                     </div>
                 ) : (
-                    <button className={styles.reply} onClick={() => setIsReplying(true)}>Reply</button>
+                    <>
+                        <button className={styles.reply} onClick={() => setIsReplying(true)}>Reply</button>
+                        {isOwner && (
+                            !isDeleted && (
+                                <button className={styles.delete} onClick={() => handleDelete(id)}>Delete Comment</button>
+                            )
+                        )}
+                    </>
                 )
             )}
             {childComments && 
@@ -76,9 +103,12 @@ export default function Comment({ id, username, commentText, createdAt, childCom
                             day: "numeric",
                         })}
                         childComments={comment.childComments ?? []}
+                        commentUserId={comment.userId}
+                        isDeleted={comment.isDeleted}
                         postId={postId}
                         userId={userId}
                         onReplyPosted={onReplyPosted}
+                        setBlogComments={setBlogComments}
                     />
                 ))
             }
