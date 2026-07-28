4744b4803150d74aa4406e67bdfe22b7999e0926
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-28 15:09:51 -0500

    fix: Bypass lightningcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssMinify: 'esbuild'
+  },
+  server: {
+    proxy: {
+      '/auth': 'http://localhost:3000'
+    }
+  }
 })
